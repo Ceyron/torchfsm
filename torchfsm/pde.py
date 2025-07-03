e82bc817d4b0d7193cf7b30958839270c2810830
qiangliu.7@outlook.com <qiangliu.7@outlook.com>
2025-07-03 11:12:10 +0200

    add GrayScott equation to the pde lib

--- torchfsm/pde.py
+++ torchfsm/pde.py
@@ -6,9 +6,14 @@ from .operator import (
     KSConvection,
     SpatialDerivative,
     VorticityConvection,
-    NSPressureConvection
+    NSPressureConvection,
+    ImplicitSource,
+    HyperDiffusion,
+    ChannelWisedDiffusion,
+    GrayScottSource,
 )
-from typing import Optional
+from typing import Optional, Union
+from torch import Tensor
 
 def Burgers(nu: float) -> Operator:
     r"""
@@ -63,7 +68,7 @@ def KortewegDeVries(dispersion_coef=1, convection_coef: float = 6.0) -> Operator
     """
     return -dispersion_coef * SpatialDerivative(0, 3) + convection_coef * Convection()
 
-def NavierStokesVorticity(Re:float,force:Optional[Operator]=None)->Operator:
+def NavierStokesVorticity(Re:Union[float,Tensor],force:Optional[Operator]=None)->Operator:
     r"""
     Navier-Stokes equation in vorticity form:
         $$\frac{\partial \omega}{\partial t} + (\mathbf{u}\cdot\nabla) \omega = \frac{1}{Re} \nabla^2 \omega + \nabla \times \mathbf{f}$$
@@ -82,7 +87,7 @@ def NavierStokesVorticity(Re:float,force:Optional[Operator]=None)->Operator:
         ns_vorticity+=force
     return ns_vorticity
 
-def NavierStokes(Re:float,force:Optional[Operator]=None)->Operator:
+def NavierStokes(Re:Union[float,Tensor],force:Optional[Operator]=None)->Operator:
     r"""
     Navier-Stokes equation:
         $$\frac{\partial\mathbf{u}}{\partial t}=-\nabla (\nabla^{-2} \nabla \cdot (\left(\mathbf{u}\cdot\nabla\right)\mathbf{u}-f))-\left(\mathbf{u}\cdot\nabla\right)\mathbf{u}+\nu \nabla^2 \mathbf{u} + \mathbf{f}$$
@@ -98,3 +103,59 @@ def NavierStokes(Re:float,force:Optional[Operator]=None)->Operator:
     """
 
     return NSPressureConvection(force)+1/Re*Laplacian()
+
+def KPPFisher(nu:Union[float,Tensor],r:Union[float,Tensor]) -> Operator:
+    r"""
+    Fisher-KPP equation:
+        $$\frac{\partial u}{\partial t} = r u (1 - u) + \nu \nabla^2 u$$
+    
+    Args:
+        nu (Union[float,Tensor]): Diffusion coefficient.
+        r (Union[float,Tensor]): Growth rate.
+
+    Returns:
+        Operator: The operator representing the Fisher-KPP equation.
+    
+    """
+    return nu*Laplacian()+r*ImplicitSource(lambda phi:phi*(1-phi))
+
+def SwiftHohenberg(r:float) -> Operator:
+    r"""
+    Swift-Hohenberg equation:
+        $$\frac{\partial \phi}{\partial t} = \nu \nabla^2 \phi + r \phi (1 - \phi)$$
+
+    Args:
+        r (float): Control parameter.
+
+    Returns:
+        Operator: The operator representing the Swift-Hohenberg equation.
+    """
+    return -HyperDiffusion()-2*Laplacian()+ImplicitSource(lambda phi:r*phi-phi+phi**2-phi**3)
+
+def GrayScott(nu_0: float, 
+              nu_1:float, 
+              feed_rate:float, 
+              kill_rate:float, 
+              de_aliasing_rate=0.5) -> Operator:
+    r"""
+    Gray-Scott equation:
+        $$\begin{aligned}
+        \frac{\partial \phi_0}{\partial t} &= \nu_0 \Delta \phi_0 - \phi_0 \phi_1^2 + f (1 - \phi_0) \\
+        \frac{\partial \phi_1}{\partial t} &= \nu_1 \Delta \phi_1 + \phi_0 \phi_1^2 - (f + k) \phi_1
+        \end{aligned}
+        $$
+
+    Args:
+        nu_0 (float): Diffusion coefficient for \(\phi_0\).
+        nu_1 (float): Diffusion coefficient for \(\phi_1\).
+        feed_rate (float): Feed rate \(f\).
+        kill_rate (float): Kill rate \(k\).
+        de_aliasing_rate (float): De-aliasing rate for the operator. Default is 0.5 due to the cubic nonlinearity.
+
+    Returns:
+        Operator: The operator representing the Gray-Scott equation.
+    """
+
+    op = ChannelWisedDiffusion([nu_0,nu_1])+ GrayScottSource(feed_rate, kill_rate)
+    op.set_de_aliasing_rate(de_aliasing_rate) # GrayScottSource requires lower de-aliasing rate due to the cubic nonlinearity
+    return op
